e81cecd1260c290709e38b8bf9218d3bca45b4c0
Saurav0808 <64957467+Saurav0808@users.noreply.github.com>
2020-05-11 10:37:37 +0530

    Update ecomm.sql

--- database/ecomm.sql
+++ database/ecomm.sql
@@ -3,10 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2018 at 03:25 PM
 -- Server version: 5.6.21
 -- PHP Version: 5.6.3
-
+<!-------------TO IMPORT AND SAVE THE DATABASE-------------->
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
